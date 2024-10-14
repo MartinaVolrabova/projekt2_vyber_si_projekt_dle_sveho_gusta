47b9b4bb863b0f90b2cbcedfe62e7295049d59d7
MartinaVolrabova <159921190+MartinaVolrabova@users.noreply.github.com>
2024-10-14 15:31:40 +0200

    Update projekt2.py
    Opravený úkol č. 2

--- projekt2.py
+++ projekt2.py
@@ -5,13 +5,25 @@ email: martina.volrabova@seznam.cz
 discord: Martina Volrabová Martina Volrabova#!pdb1714
 """
 
+
+
+"""
+projekt_2.py: druhý projekt do Engeto Online Python Akademie
+author: Martina Volrábova
+email: martina.volrabova@seznam.cz
+discord: Martina Svetr#4490
+"""
+
 import random
 
 def random_digit():
-    """Generate random 4-digit number with different digits except zero."""
-    digits = list(range(1, 9))
+    """Generate random 4-digit number with the first digit non-zero and remaining digits can include zero. Range is 0-9"""
+    digits = list(range(10))
     random.shuffle(digits)
-    return digits[:4]
+    """ Get random digit from 1 to 9 avoiding 0 as a first digit"""
+    first_digit = digits.pop(digits.index(random.choice(digits[1:]))),
+    """ return first digit with non-zero value and extend it with three random digits"""
+    return [first_digit] + digits[:3]
 
 def get_bulls_and_cows(secret, guess):
     """Return the number of Bulls and Cows for the given secret and guess."""
@@ -25,8 +37,8 @@ def is_valid_guess(guess):
         return False, "The value must contain exactly 4 digits."
     if not guess.isdigit():
         return False, "The value must contain digits only."
-    if '0' in guess:
-        return False, "The value must not contain zero."
+    if guess[0] == '0':
+        return False, "The value must not start with zero."
     if len(set(guess)) != 4:
         return False, "Digits must be unique."
     return True, ""
@@ -43,14 +55,15 @@ def play_game():
 
     while True:
         guess = input("Enter a number: ")
+        attempts += 1
         is_valid, error_message = is_valid_guess(guess)
         if not is_valid:
             print(error_message)
-            continue
+            continue 
 
         guess = [int(digit) for digit in guess]
         bulls, cows = get_bulls_and_cows(secret_number, guess)
-        attempts += 1
+
 
         bull_answer = "Bull" if bulls == 1 else "Bulls"
         cow_answer = "Cow" if cows == 1 else "Cows"
